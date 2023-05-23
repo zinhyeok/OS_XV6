ec488801946be3daef660c1d8335281e5b4b7ba1
2018015414 <2018015414@hanyang.ac.kr>
2023-05-23 22:35:02 +0900

    FEAT:[add exec2 syscall]

--- project2/user.h
+++ project2/user.h
@@ -23,6 +23,7 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+int exec2(char*, char**, int);
 
 // ulib.c
 int stat(const char*, struct stat*);
@@ -37,3 +38,4 @@ void* memset(void*, int, uint);
 void* malloc(uint);
 void free(void*);
 int atoi(const char*);
+
